abea5ef6be6eeba5bb4e376c1eede7e84f53cbc1
mingwhsun <mingwhsun@thoughtworks.com>
2018-09-08 17:20:56 +0800

    [sunming] complete PosMachine

--- src/test/java/com/cultivation/javaBasicExtended/posMachine/PosMachine.java
+++ src/test/java/com/cultivation/javaBasicExtended/posMachine/PosMachine.java
@@ -1,24 +1,75 @@
 package com.cultivation.javaBasicExtended.posMachine;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.io.IOException;
 import java.io.Reader;
-import java.util.Objects;
+import java.util.*;
 
 @SuppressWarnings({"WeakerAccess", "unused", "RedundantThrows"})
 public class PosMachine {
+    public static final String LINE_SEPARATOR = System.lineSeparator();
+    public static final String SEPARATOR_LINE = "------------------------------------------------------------" + LINE_SEPARATOR;
+    public static final String RECEIPTS_TITLE = "Receipts" + LINE_SEPARATOR;
+    public static final String PRICE = "Price: ";
+
+    Map<Product,Integer> products = new LinkedHashMap<>();
+
     public void readDataSource(Reader reader) throws IOException {
-        // TODO: please implement the following parentStaticMethod to pass the test
+        // TODO: please implement the following method to pass the test
         // <--start
-        throw new NotImplementedException();
+        try {
+            char[] readContents = new char[1000];
+            reader.read(readContents);
+            String contents = String.valueOf(readContents);
+
+            ObjectMapper mapper = new ObjectMapper();
+            List<Product> productList = new ArrayList<>();
+            productList = mapper.readValue(contents,productList.getClass());
+
+            for(Product product : productList){
+                if(!products.containsKey(product)){
+                    products.put(product,1);
+                }else {
+                    products.put(product,products.get(product)+1);
+                }
+            }
+        } catch (IOException e){
+            reader.close();
+        }
         // --end-->
     }
 
     public String printReceipt(String barcodeContent) throws IOException {
-        // TODO: please implement the following parentStaticMethod to pass the test
+        // TODO: please implement the following method to pass the test
         // <--start
-        throw new NotImplementedException();
+
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(RECEIPTS_TITLE)
+                .append(SEPARATOR_LINE);
+
+        if (barcodeContent == null || barcodeContent.length() <= 2){
+
+            sb.append(SEPARATOR_LINE)
+                    .append(PRICE + "0" + LINE_SEPARATOR);
+
+            return sb.toString();
+        }
+
+        int totalPrice = 0;
+        for(Product product : products.keySet()){
+            sb.append(product.getName()+"\t\t\t")
+                    .append(product.getPrice()+"\t")
+                    .append(products.get(product));
+            totalPrice += product.getPrice() * products.get(product);
+        }
+
+        sb.append(SEPARATOR_LINE)
+                .append(PRICE + totalPrice + LINE_SEPARATOR);
+
+        return sb.toString();
         // --end-->
     }
 }
